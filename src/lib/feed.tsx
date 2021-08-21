1f0985a5c13b5de77bb49791a1f1ca1cec99e8f1
ysmtegsr <38056766+ysmtegsr@users.noreply.github.com>
2021-08-21 17:42:11 +0900

    first commit

---
+++ src/lib/feed.tsx
@@ -0,0 +1,100 @@
+import { Feed } from 'feed'
+import fs from 'fs'
+
+type Author = {
+  name: string
+  email: string
+  link: string
+}
+
+type Event = {
+  accepted: number
+  address: string
+  catch: string
+  description: string
+  ended_at: string
+  event_id: number
+  event_type: string
+  event_url: string
+  hash_tag: string
+  lat: number | null
+  limit: number
+  lon: number | null
+  owner_display_name: string
+  owner_id: number
+  owner_nickname: string
+  place: string
+  series: {
+    id: number
+    title: string
+    url: string
+  }
+  started_at: string
+  title: string
+  updated_at: string
+  waiting: number
+}
+
+const getEvents = async () => {
+  const endpoint = 'https://connpass.com/api/v1/event/?nickname=yoshimitsu_egashira'
+  const res = await fetch(endpoint)
+  const events = await res.json()
+
+  if (!events) {
+    return { notFound: true }
+  }
+
+  return events.events
+}
+
+const generateRssFeed = async () => {
+  const baseUrl = process.env.NEXT_PUBLIC_BASE_URL || ''
+  const date = new Date()
+  const author: Author = {
+    name: 'ysmtegsr',
+    email: 'ysmtegsr@gmail.com',
+    link: 'https://ysmtegsr.com'
+  }
+
+  const feed = new Feed({
+    title: baseUrl,
+    description: 'Developer',
+    id: baseUrl,
+    link: baseUrl,
+    language: 'ja',
+    image: `${baseUrl}/favicon.png`,
+    copyright: `All rights reserved ${date.getFullYear()}, ${author.name}`,
+    updated: date,
+    feedLinks: {
+      rss2: `${baseUrl}/rss/feed/xml`,
+      atom: `${baseUrl}/rss/atom.xml`,
+      json: `${baseUrl}/rss/feed.json`
+    },
+    author
+  })
+
+  const events = await getEvents()
+
+  if (events.notFound) {
+    console.log('not found', events)
+  }
+
+  events?.forEach((e: Event) => {
+    const html: string = e.description.slice(0, 255)
+    feed.addItem({
+      title: e.title,
+      description: html,
+      id: String(e.event_id),
+      link: e.event_url,
+      content: html,
+      date: new Date(e.started_at)
+    })
+  })
+
+  fs.mkdirSync('./public/rss', { recursive: true })
+  fs.writeFileSync('./public/rss/feed.xml', feed.rss2())
+  fs.writeFileSync('./public/rss/atom.xml', feed.atom1())
+  fs.writeFileSync('./public/rss/feed.json', feed.json1())
+}
+
+export default generateRssFeed
---
+++ src/pages/hello.tsx
@@ -0,0 +1,13 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+import styles from '../styles/Home.module.css'
+
+const Hello: NextPage = () => {
+  return (
+    <div className={styles.container}>
+      hello world!
+    </div>
+  )
+}
+
+export default Hello
